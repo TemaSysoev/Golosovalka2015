d4d5d2d798a8421d81b0d2859c5c64d4dd2d72e5
Tema Sysoev <tema@MacBook-Air-Tema.local>
2014-12-05 18:56:48 +0300

    Initial Commit

---
+++ Golosovalka2015/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Golosovalka2015
+//
+//  Created by Tema Sysoev on 05/12/14.
+//  Copyright (c) 2014 Tema Sysoev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Golosovalka2015Tests/Golosovalka2015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Golosovalka2015Tests.swift
+//  Golosovalka2015Tests
+//
+//  Created by Tema Sysoev on 05/12/14.
+//  Copyright (c) 2014 Tema Sysoev. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Golosovalka2015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
